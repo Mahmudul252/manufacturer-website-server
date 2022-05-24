6ac4abb0d676c8aef2a86365e8e71990b3c7457b
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-24 22:53:48 +0600

    database created

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c1vp23d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected');
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', async (req, res) => {
     res.send('Tools Express running');
@@ -14,3 +30,4 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log("Pip Pip, Tools express running on it's track", port);
 });
+
