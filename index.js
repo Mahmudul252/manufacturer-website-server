f8cf7571f752d8ab77d1737fd84fa1ea3e72ab90
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-25 23:19:22 +0600

    order cancellation api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -54,6 +54,12 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result);
         });
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
 
     }
     finally {
