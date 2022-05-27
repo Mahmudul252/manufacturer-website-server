c8b5239e4e7cbec5c4e0e97c28f5d5fecb8e10f7
mahmudulkabir <mahmudulkabir990@gmail.com>
2022-05-27 21:42:01 +0600

    payemnt api created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
+const strip = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -41,6 +42,18 @@ async function run() {
             const query = {};
             const result = await userCollection.find(query).toArray();
             res.send(result);
+        });
+
+        app.post('/create-payment-intents', async (req, res) => {
+            const { totalPrice } = req.body;
+            console.log(totalPrice);
+            const amount = totalPrice * 100;
+            const paymentIntent = await strip.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
         });
 
         app.post('/orders', async (req, res) => {
